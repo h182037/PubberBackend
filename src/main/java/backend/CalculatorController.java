45e771145e77f9ec64f8dcca0b13f6ac6a0e0f6f
h182037 <sindre.aalhus@gmail.com>
2020-04-06 19:47:41 +0200

    api

--- src/main/java/backend/CalculatorController.java
+++ src/main/java/backend/CalculatorController.java
@@ -1,20 +1,36 @@
 package backend;
 
 import model.Bar;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 
 @RestController
+@SpringBootApplication
 public class CalculatorController {
     private final Calculator cal = new Calculator();
 
-    @GetMapping("/calculate")
+    public static void main(String[] args){
+        SpringApplication.run(CalculatorController.class, args);
+    }
+
+    @RequestMapping("/calculate")
+    @ResponseBody
     public Bar calculate(){
         if(cal.getBarlist().isEmpty())
             cal.addDummies();
         ArrayList<Bar> bars = new ArrayList<Bar>();
         bars = cal.calculate(4, 300);
         return bars.get(0);
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "hello.";
     }
 }
