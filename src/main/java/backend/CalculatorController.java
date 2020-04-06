6b427390f982884c60aff89c27b32c22eef98409
h182037 <sindre.aalhus@gmail.com>
2020-04-06 20:10:19 +0200

    simpler JSON file

--- src/main/java/backend/CalculatorController.java
+++ src/main/java/backend/CalculatorController.java
@@ -3,6 +3,9 @@ package backend;
 import model.Bar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.origin.Origin;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,12 +23,14 @@ public class CalculatorController {
 
     @RequestMapping("/calculate")
     @ResponseBody
-    public Bar calculate(){
+    public ResponseEntity<Bar> calculate(){
         if(cal.getBarlist().isEmpty())
             cal.addDummies();
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Acces-Control-Allow-Origin", "*");
         ArrayList<Bar> bars = new ArrayList<Bar>();
         bars = cal.calculate(4, 300);
-        return bars.get(0);
+        return ResponseEntity.ok().headers(responseHeaders).body(bars.get(0));
     }
 
     @RequestMapping("/")
